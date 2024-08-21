42e3230558b90cab85d1dd5cc2bdc8bf65cfea36
Nomeena <nominaabbasis@gmail.com>
2024-08-21 22:52:01 +0500

    modify

---
+++ script.js
@@ -0,0 +1,28 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    var elems = document.querySelectorAll(".elems");
+    var imgDiv = document.getElementById("img");
+  
+    elems.forEach(function (elem) {
+      // Iterate over each element
+      elem.addEventListener("mouseenter", function () {
+        let img = elem.getAttribute("data-image");
+        let w = elem.getAttribute("data-width");
+        let h = elem.getAttribute("data-height");
+  
+        imgDiv.style.backgroundImage = `url(${img})`;
+        imgDiv.style.width = w;
+        imgDiv.style.height = h;
+        imgDiv.style.display = "block"; // Show the image div
+      });
+  
+      elem.addEventListener("mouseleave", function () {
+        imgDiv.style.display = "none"; // Hide the image div
+      });
+  
+      elem.addEventListener("mousemove", function (e) {
+        imgDiv.style.left = e.pageX + 10 + "px"; // Offset to avoid flickering
+        imgDiv.style.top = e.pageY + 10 + "px"; // Offset to avoid flickering
+      });
+    });
+  });
